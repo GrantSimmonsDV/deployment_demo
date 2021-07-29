b66161d6386751c8f23ad65189d38afe9cea3202
GrantSimmonsDV <gsimmons.dv@gmail.com>
2021-07-29 12:17:26 -0600

    added endpoint get

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const path = require('path')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+
 const port = process.env.PORT || 5500
 
 
