8f27604181f6b907f5bc0accb347122909003708
GrantSimmonsDV <gsimmons.dv@gmail.com>
2021-07-29 12:00:59 -0600

    first deployment

---
+++ server/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+
+
+
+const app = express()
+
+const port = process.env.PORT || 5500
+
+
+app.listen(port, () => {console.log(`Server running on ${port}`)})
